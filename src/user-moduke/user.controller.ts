ec1d658bdde86227769f9330e15b4351a3a2bc4b
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-08-06 17:06:08 +0530

    crud user api without d

--- src/user-moduke/user.controller.ts
+++ src/user-moduke/user.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
-
+import { user } from './interface/user';
 @Controller('/user')
 export default class UserController {
   constructor(private readonly userservice: UserService) {}
   @Get()
-  getHelloUser(): string {
-    return this.userservice.getHelloUser();
+  getUser(@Body() { email }: { email: string }): string | user {
+    return this.userservice.getUser(email);
+  }
+  @Post()
+  adduser(@Body() User: user) {
+    return this.userservice.adduser(User);
+  }
+  @Get('/all')
+  allUsers() {
+    return this.userservice.getallUser();
+  }
+  @Post('/delete')
+  deleteUser(@Body() { email }) {
+    return this.userservice.deleteUser(email);
   }
 }
--- src/user-moduke/user.service.ts
+++ src/user-moduke/user.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { user } from './interface/user';
 // services are like the middlewares that will be getting the data from you like from database.
 @Injectable()
 export class UserService {
-  getHelloUser(): string {
-    return 'Hemmloo User!';
+  public users: user[] = [];
+  getUser(email: string): user | string {
+    const user = this.users.filter((user) => user.email === email);
+    return user ? user[0] : 'no  such user';
   }
-  getAmitUser(): string {
-    return ' hello amit user';
+  adduser(userinfo: user): string {
+    this.users.push(userinfo);
+    return 'Successfully added new user';
+  }
+  getallUser() {
+    return this.users;
+  }
+  deleteUser(email: string): string {
+    const user = this.users.filter((user) => user.email === email);
+    if (user) {
+      const remaininguser = this.users.filter((user) => user.email !== email);
+      this.users = remaininguser;
+      return 'Successfully deleted user';
+    }
+    return 'No Such user exist';
   }
 }
