6f004c53cb941acba4699011fa511accb252cbff
notamit <90555965+amitjimiwal@users.noreply.github.com>
2023-10-23 09:18:11 +0530

    macbook

--- src/modules/quiz/question.controller.ts
+++ src/modules/quiz/question.controller.ts
@@ -7,14 +7,19 @@ import {
 } from "@nestjs/common";
 import { QuestionDTO } from "./dto/question.dto";
 import { QuestionService } from "./question.service";
+import { QuizService } from "./quiz.service";
 
 @Controller("question")
 export class QuestionController {
-  constructor(private readonly questionService: QuestionService) {}
+  constructor(
+    private readonly questionService: QuestionService,
+    private readonly quizservice: QuizService
+  ) {}
   @Post("")
   @UsePipes(ValidationPipe)
   async saveQustion(@Body() question: QuestionDTO) {
     console.log(question);
-    return await this.questionService.createQuestion(question);
+    const quizd = this.quizservice.getQuizByID(question.id);
+    return await this.questionService.createQuestion(question,quizd);
   }
 }
